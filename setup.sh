87b332bffa81003c222ae7d6d4fa792ab00fed1c
gusteugenio <gustavoeugenio297@gmail.com>
2026-03-16 14:51:36 -0300

    fix: Corrige bug que não instalava módulos do node

--- setup.sh
+++ setup.sh
@@ -6,4 +6,4 @@ docker compose exec php composer install
 
 docker compose exec php php artisan key:generate
 
-docker compose exec php php artisan migrate --seed
+docker compose exec php php artisan migrate:fresh --seed
